6a81515761d88a0759a57169e245b04489100396
magrossmann <maxi.grossmann@hotmail.com>
2022-04-19 15:55:02 +0200

    Add Counter for Leveloccurence to Header

--- src/main/java/Header.java
+++ src/main/java/Header.java
@@ -2,9 +2,12 @@ public class Header {
     private String header;
     private int headerLevel;
 
-    public Header(String header, int headerLevel) {
+    private int numInLevel;
+
+    public Header(String header, int headerLevel, int numInLevel) {
         this.header = header;
         this.headerLevel = headerLevel;
+        this.numInLevel = numInLevel;
     }
 
     public String getHeader() {
@@ -21,5 +24,13 @@ public class Header {
 
     public void setHeaderLevel(int headerLevel) {
         this.headerLevel = headerLevel;
+    }
+
+    public int getNumInLevel() {
+        return numInLevel;
+    }
+
+    public void setNumInLevel(int numInLevel) {
+        this.numInLevel = numInLevel;
     }
 }
