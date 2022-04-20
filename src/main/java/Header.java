d162461911c9ac2a3728c64f290f97290b376598
magrossmann <maxi.grossmann@hotmail.com>
2022-04-20 10:24:47 +0200

    Delete unused Constructor in Header

--- src/main/java/Header.java
+++ src/main/java/Header.java
@@ -3,21 +3,12 @@ import java.util.stream.Stream;
 public class Header {
     private String header;
     private int headerLevel;
-
     private String headerLevelString;
-
-    private int numInLevel;
 
     public Header(String header, int headerLevel, String numInLevel) {
         this.header = header;
         this.headerLevel = headerLevel;
         this.headerLevelString = numInLevel;
-    }
-
-    public Header(String header, int headerLevel, int numInLevel) {
-        this.header = header;
-        this.headerLevel = headerLevel;
-        this.numInLevel = numInLevel;
     }
 
     public String getHeader() {
