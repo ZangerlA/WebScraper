53eebb247e827e19a8d07ac1b9331033295faead
magrossmann <maxi.grossmann@hotmail.com>
2022-04-25 15:21:40 +0200

    Implementation of MultiLevelIndexTests

--- src/test/java/MultiLevelIndexTests.java
+++ src/test/java/MultiLevelIndexTests.java
@@ -1,7 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MultiLevelIndexTests {
 
+    MultiLevelIndex multiLevelIndex;
 
-    //nextIndex higher lower same
-    //toString
-    //constructor default/int array
+    @BeforeEach
+    void initializeMultiLevelIndex(){
+        multiLevelIndex = new MultiLevelIndex();
+    }
+
+    @Test
+    void WhenTwiceNextIndexWithIncreasedIndexLevelDepthsThen(){
+        assertEquals("1.1.1", multiLevelIndex.nextIndex(1).nextIndex(2).toString());
+    }
+    @Test
+    void WhenFirstNextIndexWithHigherIndexLevelDepthSecondNextIndexWithLowerIndexLevelDepthThen(){
+        assertEquals("1.2", multiLevelIndex.nextIndex(2).nextIndex(1).toString());
+    }
+    @Test
+    void WhenTwiceNextIndexWithSameIndexLevelDepthsThen(){
+        assertEquals("2", multiLevelIndex.nextIndex(0).nextIndex(0).toString());
+    }
+    @Test
+    void WhenConstructorWithArrayAsParameterThen(){
+        MultiLevelIndex multiLevelIndexWithArray = new MultiLevelIndex(new int[]{1,3,1,2});
+        assertEquals("1.3.1.2", multiLevelIndexWithArray.toString());
+    }
 }
