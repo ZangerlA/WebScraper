566d39fd67bac16ed813e3bf475d18c81ae51573
magrossmann <maxi.grossmann@hotmail.com>
2022-06-06 16:10:06 +0200

    Update webscrapertests

--- src/test/java/WebScraperTests.java
+++ src/test/java/WebScraperTests.java
@@ -8,17 +8,11 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WebScraperTests {
-    WebScraper webScraperWithUrlDepth;
-    WebScraper webScraperWithUrlDepthLanguage;
-    WebScraper webScraperWithUrlDepthFile;
     WebScraper webScraperWithUrlDepthLanguageFile;
     final String fileTestName = "default.md";
 
     @BeforeEach
     public void init() {
-        webScraperWithUrlDepth = new WebScraper("quotes.toscrape.com/", 1);
-        webScraperWithUrlDepthLanguage = new WebScraper("https://teamkill.at/",1, Language.DE);
-        webScraperWithUrlDepthFile = new WebScraper("https://quotes.toscrape.com/",1, fileTestName);
         webScraperWithUrlDepthLanguageFile = new WebScraper("https://teamkill.at/", 1, fileTestName, Language.EN);
     }
 
@@ -30,32 +24,29 @@ public class WebScraperTests {
 
     @Test
     public void WhenValidArgumentsThenShouldNotThrow() {
-        assertDoesNotThrow(()-> webScraperWithUrlDepth.scrape());
-        assertDoesNotThrow(()-> webScraperWithUrlDepthLanguage.scrape());
-        assertDoesNotThrow(()-> webScraperWithUrlDepthFile.scrape());
         assertDoesNotThrow(()-> webScraperWithUrlDepthLanguageFile.scrape());
     }
 
     @Test
     public void WhenValidArgumentsThenScraperShouldFinish() {
-        assertTimeout(Duration.ofMinutes(1), ()-> webScraperWithUrlDepth.scrape());
+        assertTimeout(Duration.ofMinutes(1), ()-> webScraperWithUrlDepthLanguageFile.scrape());
     }
 
     @Test
     public void WhenValidArgumentsThenFileShouldExist() {
-        webScraperWithUrlDepthFile.scrape();
+        webScraperWithUrlDepthLanguageFile.scrape();
         File file = new File(fileTestName);
         assertTrue(file.exists());
     }
 
     @Test
     public void WhenSearchDepthNegativeThenShouldNotThrow(){
-        webScraperWithUrlDepth = new WebScraper("abc", -1);
-        assertDoesNotThrow(()-> webScraperWithUrlDepth.scrape());
+        webScraperWithUrlDepthLanguageFile = new WebScraper("abc", -1, fileTestName , Language.EN);
+        assertDoesNotThrow(()-> webScraperWithUrlDepthLanguageFile.scrape());
     }
     @Test
     public void WhenInvalidFileNameThenDoesNotThrow(){
-        webScraperWithUrlDepth = new WebScraper("https://quotes.toscrape.com/",1, "//.");
-        assertDoesNotThrow(()-> webScraperWithUrlDepth.scrape());
+        webScraperWithUrlDepthLanguageFile = new WebScraper("https://quotes.toscrape.com/",1, "//.", Language.EN);
+        assertDoesNotThrow(()-> webScraperWithUrlDepthLanguageFile.scrape());
     }
 }
